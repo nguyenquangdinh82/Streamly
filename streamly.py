be02aaf04472ef14c6612bcae8e677b46330ed22
nguyenquangdinh82 <33990195+nguyenquangdinh82@users.noreply.github.com>
2024-11-14 04:54:25 +0000

    onlychat

--- streamly.py
+++ streamly.py
@@ -1,12 +1,10 @@
-import openai
 import streamlit as st
 import logging
-from PIL import Image, ImageEnhance
 import time
 import json
-import requests
 import base64
-from openai import OpenAI, OpenAIError
+from PIL import Image, ImageEnhance
+import requests
 
 import google.generativeai as genai
 
@@ -19,7 +17,7 @@ def configure_api():
         st.stop()
     genai.configure(api_key=GOOGLE_API_KEY)
     system_prompt = """You are Streamly, a specialized AI assistant trained in Streamlit.
-        Streamly, is powered by the OpenAI GPT-4o-mini model, released on July 18, 2024.
+        Streamly, is powered by the Gemini model.
         You are trained up to Streamlit Version 1.36.0, release on June 20, 2024.
         Refer to conversation history to provide context to your response.
         You were created by Madie Laine, an ggAI Researcher."""
@@ -33,8 +31,8 @@ configure_api()  # Thiết lập API khi khởi động
 # 2. Hàm xử lý yêu cầu và phản hồi
 def generate_response(request, histori):
     chat = model.start_chat(
-    history=histori
-)
+        history=histori
+        )
     
     response = chat.send_message(request)
     return response.text.strip()
@@ -115,36 +113,6 @@ def load_and_enhance_image(image_path, enhance=False):
         img = enhancer.enhance(1.8)
     return img
 
-@st.cache_data(show_spinner=False)
-def load_streamlit_updates():
-    """Load the latest Streamlit updates from a local JSON file."""
-    try:
-        with open("data/streamlit_updates.json", "r") as f:
-            return json.load(f)
-    except (FileNotFoundError, json.JSONDecodeError) as e:
-        logging.error(f"Error loading JSON: {str(e)}")
-        return {}
-
-def get_streamlit_api_code_version():
-    """
-    Get the current Streamlit API code version from the Streamlit API documentation.
-
-    Returns:
-    - str: The current Streamlit API code version.
-    """
-    try:
-        response = requests.get(API_DOCS_URL)
-        if response.status_code == 200:
-            return "1.36"
-    except requests.exceptions.RequestException as e:
-        logging.error(f"Error connecting to the Streamlit API documentation: {str(e)}")
-    return None
-
-def display_streamlit_updates():
-    """Display the latest updates of the Streamlit."""
-    with st.expander("Streamlit 1.36 Announcement", expanded=False):
-        st.markdown("For more details on this version, check out the [Streamlit Forum post](https://docs.streamlit.io/library/changelog#version).")
-
 def initialize_conversation():
     """
     Initialize the conversation history with system and assistant messages.
@@ -159,85 +127,16 @@ def initialize_conversation():
     return conversation_history
 
 @st.cache_data(show_spinner=False)
-def get_latest_update_from_json(keyword, latest_updates):
-    """
-    Fetch the latest Streamlit update based on a keyword.
-
-    Parameters:
-    - keyword (str): The keyword to search for in the Streamlit updates.
-    - latest_updates (dict): The latest Streamlit updates data.
-
-    Returns:
-    - str: The latest update related to the keyword, or a message if no update is found.
-    """
-    for section in ["Highlights", "Notable Changes", "Other Changes"]:
-        for sub_key, sub_value in latest_updates.get(section, {}).items():
-            for key, value in sub_value.items():
-                if keyword.lower() in key.lower() or keyword.lower() in value.lower():
-                    return f"Section: {section}\nSub-Category: {sub_key}\n{key}: {value}"
-    return "No updates found for the specified keyword."
-
-def construct_formatted_message(latest_updates):
-    """
-    Construct formatted message for the latest updates.
-
-    Parameters:
-    - latest_updates (dict): The latest Streamlit updates data.
-
-    Returns:
-    - str: Formatted update messages.
-    """
-    formatted_message = []
-    highlights = latest_updates.get("Highlights", {})
-    version_info = highlights.get("Version 1.36", {})
-    if version_info:
-        description = version_info.get("Description", "No description available.")
-        formatted_message.append(f"- **Version 1.36**: {description}")
-
-    for category, updates in latest_updates.items():
-        formatted_message.append(f"**{category}**:")
-        for sub_key, sub_values in updates.items():
-            if sub_key != "Version 1.36":  # Skip the version info as it's already included
-                description = sub_values.get("Description", "No description available.")
-                documentation = sub_values.get("Documentation", "No documentation available.")
-                formatted_message.append(f"- **{sub_key}**: {description}")
-                formatted_message.append(f"  - **Documentation**: {documentation}")
-    return "\n".join(formatted_message)
-
-@st.cache_data(show_spinner=False)
-def on_chat_submit(chat_input, latest_updates):
-    """
-    Handle chat input submissions and interact with the OpenAI API.
-
-    Parameters:
-    - chat_input (str): The chat input from the user.
-    - latest_updates (dict): The latest Streamlit updates fetched from a JSON file or API.
-
-    Returns:
-    - None: Updates the chat history in Streamlit's session state.
-    """
+def on_chat_submit(chat_input):
     user_input = chat_input.strip().lower()
 
     if 'conversation_history' not in st.session_state:
         st.session_state.conversation_history = initialize_conversation()
 
-    assistant_reply = ""
-
-    if "latest updates" in user_input:
-        assistant_reply = "Here are the latest highlights from Streamlit:\n"
-        highlights = latest_updates.get("Highlights", {})
-        if highlights:
-            for version, info in highlights.items():
-                description = info.get("Description", "No description available.")
-                assistant_reply += f"- **{version}**: {description}\n"
-        else:
-            assistant_reply = "No highlights found."
-    else:
-        response = generate_response(user_input,st.session_state.conversation_history)
-        assistant_reply = response
+    response = generate_response(user_input, st.session_state.conversation_history)
+    assistant_reply = response
 
     st.session_state.conversation_history.append({"role": "user", "parts": user_input})
-
     st.session_state.conversation_history.append({"role": "model", "parts": assistant_reply})
     st.session_state.history.append({"role": "user", "parts": user_input})
     st.session_state.history.append({"role": "model", "parts": assistant_reply})
@@ -296,7 +195,7 @@ def main():
     st.sidebar.markdown("---")
 
     # Sidebar for Mode Selection
-    mode = st.sidebar.radio("Select Mode:", options=["Latest Updates", "Chat with Streamly"], index=1)
+    mode = st.sidebar.radio("Select Mode:", options=["Chat with Streamly"], index=0)
 
     st.sidebar.markdown("---")
 
@@ -307,7 +206,6 @@ def main():
         ### Basic Interactions
         - **Ask About Streamlit**: Type your questions about Streamlit's latest updates, features, or issues.
         - **Search for Code**: Use keywords like 'code example', 'syntax', or 'how-to' to get relevant code snippets.
-        - **Navigate Updates**: Switch to 'Updates' mode to browse the latest Streamlit updates in detail.
         """)
 
     # Display advanced interactions
@@ -335,8 +233,7 @@ def main():
     if mode == "Chat with Streamly":
         chat_input = st.chat_input("Ask me about Streamlit updates:")
         if chat_input:
-            latest_updates = load_streamlit_updates()
-            on_chat_submit(chat_input, latest_updates)
+            on_chat_submit(chat_input)
 
         # Display chat history
         for message in st.session_state.history[-NUMBER_OF_MESSAGES_TO_DISPLAY:]:
@@ -345,8 +242,7 @@ def main():
             with st.chat_message(role, avatar=avatar_image):
                 st.write(message["parts"])
 
-    else:
-        display_streamlit_updates()
+    
 
 if __name__ == "__main__":
     main()
